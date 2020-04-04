4148b022ee34736b5e2792df18df7a8dcd8cfc45
Jaimie van Santen <j.vansanten@coolblue.nl>
2020-04-05 01:26:33 +0200

    Make address configurable

--- cypress/integration/zakkenservice/request.spec.js
+++ cypress/integration/zakkenservice/request.spec.js
@@ -1,3 +1,6 @@
+
+const URL = 'https://www.zakkenservice.nl';
+
 Cypress.on('uncaught:exception', (err, runnable) => {
     // returning false here prevents Cypress from
     // failing the test
@@ -8,14 +11,14 @@ Cypress.on('uncaught:exception', (err, runnable) => {
 
 describe('Zakkenservice', () => {
     it('Requests new PMD bags', () => {
-        cy.visit('https://www.zakkenservice.nl');
+        cy.visit(URL);
 
-        fillInput('[name="ctl00$MainContent$TextBox1"]', 'POSTALCODE');
-        fillInput('[name="ctl00$MainContent$TextBox2"]', 'HOUSENUMBER');
+        fillInput('[name="ctl00$MainContent$TextBox1"]', Cypress.env('postal_code'));
+        fillInput('[name="ctl00$MainContent$TextBox2"]', Cypress.env('house_number'));
 
         cy.get('#ctl00_MainContent_Label4')
-            .contains('Postcode gecontroleerd, druk op bestellen.')
-            .should('be.visible');
+          .contains('Postcode gecontroleerd, druk op bestellen.')
+          .should('be.visible');
 
         cy.get('[type=submit]').first().click();
         cy.screenshot();
